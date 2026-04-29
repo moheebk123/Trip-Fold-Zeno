60abad76f4cae3872a303eb337fa67e918d3f748
Moheeb <moheebk522@gmail.com>
2026-04-29 16:24:30 +0530

    add header

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="mb-2 text-center">
+      <h1 className="text-3xl font-bold tracking-tight">
+        Trip Expense Calculator
+      </h1>
+      <p className="text-slate-400 mt-2 text-lg">Manage group expenses smartly</p>
+    </div>
+  );
+}
+
+export default Header
