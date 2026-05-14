fc2bfaf3bd5174517f758ba7a8383fce02a4eef0
Moheeb <moheebk522@gmail.com>
2026-05-15 01:35:53 +0530

    add pages in index

---
+++ src/pages/index.ts
@@ -0,0 +1,23 @@
+// General Pages
+import Home from "./general/Home";
+import Support from "./general/Support";
+// Trip Pages
+import Trips from "./trip/Trips";
+import Trip from "./trip/Trip";
+// Error Page
+import NotFound from "./error/NotFound";
+// Legal Pages
+import PrivacyPolicy from "./legal/PrivacyPolicy";
+import SecurityPolicy from "./legal/SecurityPolicy";
+import TermsOfService from "./legal/TermsOfService";
+
+export {
+  Home,
+  Trips,
+  Trip,
+  Support,
+  NotFound,
+  PrivacyPolicy,
+  SecurityPolicy,
+  TermsOfService,
+};
